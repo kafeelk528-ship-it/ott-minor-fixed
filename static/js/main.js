aa7b02f636a7c3597255dfa33909d11eb2b1b0cb
kafeelk528-ship-it <kafeelk528@gmail.com>
2025-11-21 20:45:27 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
   // you can add UI interactions here
   console.log("main.js loaded");
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  const revealOnScroll = () => {
+    cards.forEach(card => {
+      const cardTop = card.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (cardTop < windowHeight - 50) {
+        card.classList.add("show-card");
+      }
+    });
+  };
+
+  window.addEventListener("scroll", revealOnScroll);
+  revealOnScroll();
+});
+
